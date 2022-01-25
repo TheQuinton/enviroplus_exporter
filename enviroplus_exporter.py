830c15e74f3d22d99ffb1ccbb3fe8a34d1ff02c7
quisch <quinton.schafer@kambi.com>
2022-01-25 00:15:57 -0500

    Added AQI calculation and added cpu temp output

--- enviroplus_exporter.py
+++ enviroplus_exporter.py
@@ -6,14 +6,17 @@ import time
 import logging
 import argparse
 import subprocess
+import aqi
 from threading import Thread
 
 from prometheus_client import start_http_server, Gauge, Histogram
 
 from bme280 import BME280
 from enviroplus import gas
-from pms5003 import PMS5003, ReadTimeoutError as pmsReadTimeoutError
-
+from pms5003 import PMS5003
+from pms5003 import ReadTimeoutError as pmsReadTimeoutError
+from pms5003 import SerialTimeoutError as pmsSerialTimeoutError
+from pms5003 import ChecksumMismatchError as pmsChecksumMismatchError
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -60,6 +63,8 @@ PROXIMITY = Gauge('proximity', 'proximity, with larger numbers being closer prox
 PM1 = Gauge('PM1', 'Particulate Matter of diameter less than 1 micron. Measured in micrograms per cubic metre (ug/m3)')
 PM25 = Gauge('PM25', 'Particulate Matter of diameter less than 2.5 microns. Measured in micrograms per cubic metre (ug/m3)')
 PM10 = Gauge('PM10', 'Particulate Matter of diameter less than 10 microns. Measured in micrograms per cubic metre (ug/m3)')
+AQI = Gauge('AQI', 'AQI is calculated according to the recommandations of the United States Environmental Protection Agency')
+CPU_TEMPERATURE = Gauge('cpu_temperature','CPU temperature measured (*C)')
 
 OXIDISING_HIST = Histogram('oxidising_measurements', 'Histogram of oxidising measurements', buckets=(0, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000, 80000, 85000, 90000, 100000))
 REDUCING_HIST = Histogram('reducing_measurements', 'Histogram of reducing measurements', buckets=(0, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000, 1100000, 1200000, 1300000, 1400000, 1500000))
@@ -68,6 +73,7 @@ NH3_HIST = Histogram('nh3_measurements', 'Histogram of nh3 measurements', bucket
 PM1_HIST = Histogram('pm1_measurements', 'Histogram of Particulate Matter of diameter less than 1 micron measurements', buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
 PM25_HIST = Histogram('pm25_measurements', 'Histogram of Particulate Matter of diameter less than 2.5 micron measurements', buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
 PM10_HIST = Histogram('pm10_measurements', 'Histogram of Particulate Matter of diameter less than 10 micron measurements', buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
+#AQI_HIST = Histogram('aqi_measurements', 'Histogram of AQI calculated using PM25 and PM10 measurements')
 
 # Setup InfluxDB
 # You can generate an InfluxDB Token from the Tokens Tab in the InfluxDB Cloud UI
@@ -91,10 +97,11 @@ def reset_i2c():
 
 # Get the temperature of the CPU for compensation
 def get_cpu_temperature():
+    """Get the temperature from the Raspberry Pi CPU"""
     with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
         temp = f.read()
         temp = int(temp) / 1000.0
-    return temp
+        CPU_TEMPERATURE.set(temp)
 
 def get_temperature(factor):
     """Get temperature from the weather sensor"""
@@ -123,10 +130,13 @@ def get_pressure():
         logging.error("Could not get pressure readings. Resetting i2c.")
         reset_i2c()
 
-def get_humidity():
+def get_humidity(humidity_compensation):
     """Get humidity from the weather sensor"""
     try:
         humidity = bme280.get_humidity()
+        if humidity_compensation:
+            humidity = humidity + humidity_compensation
+
         HUMIDITY.set(humidity)
     except IOError:
         logging.error("Could not get humidity readings. Resetting i2c.")
@@ -152,11 +162,11 @@ def get_gas():
 def get_light():
     """Get all light readings"""
     try:
-       lux = ltr559.get_lux()
-       prox = ltr559.get_proximity()
+        lux = ltr559.get_lux()
+        prox = ltr559.get_proximity()
 
-       LUX.set(lux)
-       PROXIMITY.set(prox)
+        LUX.set(lux)
+        PROXIMITY.set(prox)
     except IOError:
         logging.error("Could not get lux and proximity readings. Resetting i2c.")
         reset_i2c()
@@ -165,7 +175,7 @@ def get_particulates():
     """Get the particulate matter readings"""
     try:
         pms_data = pms5003.read()
-    except pmsReadTimeoutError:
+    except (pmsReadTimeoutError, pmsSerialTimeoutError, pmsChecksumMismatchError):
         logging.warning("Failed to read PMS5003")
     except IOError:
         logging.error("Could not get particulate matter readings. Resetting i2c.")
@@ -178,6 +188,18 @@ def get_particulates():
         PM1_HIST.observe(pms_data.pm_ug_per_m3(1.0))
         PM25_HIST.observe(pms_data.pm_ug_per_m3(2.5) - pms_data.pm_ug_per_m3(1.0))
         PM10_HIST.observe(pms_data.pm_ug_per_m3(10) - pms_data.pm_ug_per_m3(2.5))
+
+def get_aqi():
+    """Calculate AQI from the PMS5003 sensor readings"""
+    try:
+        pms_data = pms5003.read()
+        aqi_calc = aqi.to_aqi([
+            (aqi.POLLUTANT_PM25, pms_data.pm_ug_per_m3(2.5)),
+            (aqi.POLLUTANT_PM10, pms_data.pm_ug_per_m3(10))])
+        AQI.set(aqi_calc)
+    except IOError:
+        logging.error("Could not calculate AQI readings. Resetting i2c.")
+        reset_i2c() 
 
 def collect_all_data():
     """Collects all the data currently set"""
@@ -193,6 +215,8 @@ def collect_all_data():
     sensor_data['pm1'] = PM1.collect()[0].samples[0].value
     sensor_data['pm25'] = PM25.collect()[0].samples[0].value
     sensor_data['pm10'] = PM10.collect()[0].samples[0].value
+    sensor_data['aqi'] = AQI.collect()[0].samples[0].value
+    sensor_data['cpu_temperature'] = CPU_TEMPERATURE.collect()[0].samples[0].value
     return sensor_data
 
 def post_to_influxdb():
@@ -284,8 +308,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--bind", metavar='ADDRESS', default='0.0.0.0', help="Specify alternate bind address [default: 0.0.0.0]")
     parser.add_argument("-p", "--port", metavar='PORT', default=8000, type=int, help="Specify alternate port [default: 8000]")
+    parser.add_argument("-e", "--enviro", metavar='ENVIRO', type=str_to_bool, default='false', help="Device is an Enviro (not Enviro+) so don't fetch data from gas and particulate sensors as they don't exist")
     parser.add_argument("-f", "--factor", metavar='FACTOR', type=float, help="The compensation factor to get better temperature results when the Enviro+ pHAT is too close to the Raspberry Pi board")
-    parser.add_argument("-e", "--enviro", metavar='ENVIRO', type=str_to_bool, help="Device is an Enviro (not Enviro+) so don't fetch data from gas and particulate sensors as they don't exist")
+    parser.add_argument("-u", "--humid", metavar='HUMIDITY', type=float, help="The humidity compensation value to get better humidity results when the Enviro+ pHAT is too close to the Raspberry Pi board")
     parser.add_argument("-d", "--debug", metavar='DEBUG', type=str_to_bool, help="Turns on more verbose logging, showing sensor output and post responses [default: false]")
     parser.add_argument("-i", "--influxdb", metavar='INFLUXDB', type=str_to_bool, default='false', help="Post sensor data to InfluxDB [default: false]")
     parser.add_argument("-l", "--luftdaten", metavar='LUFTDATEN', type=str_to_bool, default='false', help="Post sensor data to Luftdaten [default: false]")
@@ -300,6 +325,9 @@ if __name__ == '__main__':
 
     if args.factor:
         logging.info("Using compensating algorithm (factor={}) to account for heat leakage from Raspberry Pi board".format(args.factor))
+
+    if args.humid:
+        logging.info("Using humidity compensation, increasing the output value by {}% to account for heat leakage from Raspberry Pi board".format(args.humid))
 
     if args.influxdb:
         # Post to InfluxDB in another thread
@@ -318,11 +346,13 @@ if __name__ == '__main__':
 
     while True:
         get_temperature(args.factor)
+        get_humidity(args.humid)
         get_pressure()
-        get_humidity()
         get_light()
+        get_cpu_temperature()
         if not args.enviro:
             get_gas()
             get_particulates()
+            get_aqi()
         if DEBUG:
             logging.info('Sensor data: {}'.format(collect_all_data()))
